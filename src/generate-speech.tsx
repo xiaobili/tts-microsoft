c77113efea260228ba7f85b2013c018cab80f9cb
billy <zygboom@163.com>
2023-04-12 21:58:57 +0800

    add stop voice func

--- src/generate-speech.tsx
+++ src/generate-speech.tsx
@@ -1,59 +1,68 @@
-import { getSelectedText, showToast, Toast, showHUD, getPreferenceValues } from "@raycast/api";
+import { getSelectedText, showToast, Toast, showHUD, getPreferenceValues, Cache } from "@raycast/api";
 import fs from "fs";
 import { getNowTime } from "./utils/date";
-import { exec } from "child_process";
+import { ChildProcess, exec } from "child_process";
 import request from "./utils/axios";
 
 interface Preferences {
-    voice: string;
-    fileDirectory: string;
-    openDirectory: boolean;
-    api: string;
+  voice: string;
+  fileDirectory: string;
+  openDirectory: boolean;
+  api: string;
+}
+
+export default async () => {
+  const preferences: Preferences = getPreferenceValues();
+
+  let selectedText;
+  try {
+    selectedText = await getSelectedText();
+  } catch (e) {
+    await showHUD("请确保您已选择要生成音频的文本");
+    return;
   }
-  
-  export default async () => {
-    const preferences: Preferences = getPreferenceValues();
-  
-    let selectedText;
-    try {
-      selectedText = await getSelectedText();
-    } catch (e) {
-      await showHUD("请确保您已选择要生成音频的文本");
-      return;
-    }
-  
-    const { voice, fileDirectory, openDirectory, api } = preferences;
-  
-    const template = `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts"
+
+  const { voice, fileDirectory, openDirectory, api } = preferences;
+
+  const template = `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts"
                           xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
                           <voice name="${voice}">
                               <prosody rate="0%" pitch="0%">${selectedText}</prosody>
                           </voice>
-                      </speak>`
-  
-    await showToast(Toast.Style.Animated, "Generating  Voice...");
-  
-    const res = await request.post(api, template);
-  
-    const { data } = res;
-  
-    const nowTime = getNowTime();
-  
-    const fileName = `${nowTime}.mp3`;
-  
-    const filePath = `${fileDirectory}/${fileName}`;
-  
-    // 生成音频文件
-    fs.writeFileSync(filePath, data);
-  
-    // 打开文件夹
-    if (openDirectory) {
-      exec(`open ${fileDirectory}`);
-      exec(`afplay ${filePath}`);
-    } else {
-        exec(`afplay ${filePath}`);
-    }
-  
-    await showToast(Toast.Style.Success, "Generate Voice Success");
-  };
-  
+                      </speak>`;
+
+  await showToast(Toast.Style.Animated, "Generating  Voice...");
+
+  const res = await request.post(api, template);
+
+  const { data } = res;
+
+  const nowTime = getNowTime();
+
+  const fileName = `${nowTime}.mp3`;
+
+  const filePath = `${fileDirectory}/${fileName}`;
+
+  // 生成音频文件
+  fs.writeFileSync(filePath, data);
+
+  let playmusic: ChildProcess | null = null;
+  // 打开文件夹
+  if (openDirectory) {
+    exec(`open ${fileDirectory}`);
+    exec(`afplay ${filePath}`);
+  } else {
+    playmusic = exec(`afplay ${filePath}`);
+    // 关闭音频监听
+    playmusic.on("exit", function (code, signal) {
+      console.log("child process exited with " + `code ${code} and signal ${signal}`);
+    });
+  }
+
+  const cache = new Cache();
+  if (playmusic) {
+    cache.set("playmusic", playmusic.pid?.toString() || "");
+  }
+
+  await showToast(Toast.Style.Success, "Generate Voice Success");
+};
---
+++ src/kill-speech.tsx
@@ -0,0 +1,14 @@
+import { exec } from "child_process";
+import { Cache, showHUD } from "@raycast/api";
+
+export default async () => {
+  const cache = new Cache();
+  const playmusic: String | null = cache.get("playmusic") || null;
+  if (playmusic) {
+    exec(`kill -9 ${playmusic}`);
+    cache.remove("playmusic");
+    await showHUD("音频已停止");
+  } else {
+    await showHUD("没有正在播放的音频");
+  }
+};
